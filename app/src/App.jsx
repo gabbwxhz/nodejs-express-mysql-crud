ad90c7d2ed59d1dbb3ad0b27c0a135033082b2d7
gabbwhxz <vnvsgabe@gmail.com>
2024-06-18 14:54:41 -0300

    adicionado rotas

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,53 +1,15 @@
-/* eslint-disable no-unused-vars */
-import { useState, useEffect } from 'react'
-
-import styled from 'styled-components'
-import axios from 'axios'
-
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { Routes, Route } from 'react-router-dom'
 
 import GlobalStyle from './GlobalStyle'
 
-import Form from './components/Form'
-import Grid from './components/Grid'
-
-const Container = styled.div`
-   width: 1000px;
-   margin-top: 20px;
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   gap: 10px;
-`
-
-const Title = styled.h1``
+import Costumers from './pages/Costumers'
 
 export default function App() {
-   const [users, setUsers] = useState([])
-   const [onEdit, setOnEdit] = useState(null)
-
-   const getUsers = async () => {
-      try {
-         const res = await axios.get('http://localhost:8800')
-         setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)))
-      } catch (err) {
-         toast.error(err)
-      }
-   }
-
-   useEffect(() => {
-      getUsers()
-   }, [setUsers])
-
    return (
       <>
-         <Container>
-            <Title>TESTE CLIENTES</Title>
-            <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
-            <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
-         </Container>
-         <ToastContainer autoClose={3000} position="bottom-left" />
+         <Routes>
+            <Route path="/costumers" element={<Costumers />} />
+         </Routes>
          <GlobalStyle />
       </>
    )
