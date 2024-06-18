5e49158dafea4b27bd99c32bf94815e3a8f355df
gabbwhxz <vnvsgabe@gmail.com>
2024-06-18 17:56:51 -0300

    utilizado rotas no index

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 
-import userRoutes from './routes/users.js'
+import customerRoutes from './routes/customers.js'
+import employeeRoutes from './routes/employees.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/', userRoutes)
+app.use('/customers', customerRoutes)
+app.use('/employees', employeeRoutes)
 
 app.listen(8800)
