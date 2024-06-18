e7f47586b8e939f060db647faedd0cb6b21c1e0d
gabbwhxz <vnvsgabe@gmail.com>
2024-06-18 15:58:03 -0300

    adicionado rota para pagina de funcionarias

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import GlobalStyle from './GlobalStyle'
 
-import Costumers from './pages/Costumers'
+import Customers from './pages/Customers'
+import Employees from './pages/Employees'
 
 export default function App() {
    return (
       <>
          <Routes>
-            <Route path="/costumers" element={<Costumers />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/employees" element={<Employees />} />
          </Routes>
          <GlobalStyle />
       </>
