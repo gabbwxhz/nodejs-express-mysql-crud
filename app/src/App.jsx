253414109c1526070c26f230095e6903f5fe7fc1
gabbwhxz <vnvsgabe@gmail.com>
2024-06-19 23:59:14 -0300

    adicionado pagina de serviços

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import GlobalStyle from './GlobalStyle'
 
 import Customers from './pages/Customers'
 import Employees from './pages/Employees'
+import Services from './pages/Services'
 
 export default function App() {
    return (
@@ -11,6 +12,7 @@ export default function App() {
          <Routes>
             <Route path="/customers" element={<Customers />} />
             <Route path="/employees" element={<Employees />} />
+            <Route path="/services" element={<Services />} />
          </Routes>
          <GlobalStyle />
       </>
